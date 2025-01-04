fff8a5131913310912371fc1ebc491761db05595
Quaternions <krakow20@gmail.com>
2025-01-04 01:38:51 -0800

    main Result type is singular

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // #![windows_subsystem = "windows"] // uncomment this to suppress terminal on windows
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), winit::error::EventLoopError> {
     let event_loop = winit::event_loop::EventLoop::builder().build()?;
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
     let mut app = app_core::App::default();
